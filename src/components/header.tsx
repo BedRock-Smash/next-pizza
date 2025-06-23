1aa7e299a5158903562df861dc13240121483f75
Rafael2378 <rafaelstepanyan036@gmail.com>
2025-06-23 18:36:26 +0400

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,33 @@
-import React from "react";
-import { cn } from "@/lib/utils";
-import { Button } from "./ui/button";
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { Button } from '@ui/button';
+import { Logo } from '@/components';
+import { Search } from './search';
+import { Container } from './container';
 
 interface Props {
-  className?: string;
+    className?: string;
+
+
 }
 
 export const Header: React.FC<Props> = (props) => {
-  const { className } = props;
+    const { className } = props;
+    return (
+        <header className={cn("", className)}>
+            <Container className='flex items-center gap-3'>
+                <Logo />
+                <Search className='flex-1' />
+                <Button variant={"outline"}>
 
-  return (
-    <header className={cn("", className)}>
-      <Button>barev</Button>
-    </header>
-  );
-};
+                    <span>Войти</span>
+                </Button>
+                <Button>
+                    <span>520$</span>
+                    |
+                    <span>3</span>
+                </Button>
+            </Container>
+        </header>
+    );
+}
